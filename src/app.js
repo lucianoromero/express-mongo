53e88ab4c5bb63e701fe9d8c53c553c28c7df179
Luciano Luiz Romero <lucianoluizromero@gmail.com>
2024-06-20 20:47:26 -0300

    Implementando roda de retorno livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;

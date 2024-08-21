6506ff8087497138a2043047c7f4d4790af58e76
Luciano Luiz Romero <lucianoluizromero@gmail.com>
2024-08-20 21:22:31 -0300

    Implementando schema livro e get find no mongo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import conectaNaDatabase from "./config/dbConnect.js";
+import livro  from "./models/Livro.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -14,23 +15,13 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        título: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        título: "O Hobbit"
-    }
-];
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.post("/livros", (req, res) => {
@@ -54,17 +45,5 @@ app.delete('/livros/:id', (req, res) => {
     livros.splice(index, 1);
     res.status(200).send('Livro removido com sucesso.');
 });
-
-function buscaLivroIndex(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
-
-function buscaLivro(id) {
-    const livro = livros.find(livro => livro.id === Number(id));
-    return livro || "Livro não encontrado";
-}
 
 export default app;
